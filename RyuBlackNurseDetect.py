bddede312ea45c35b8a6235da3f5e2bbadd63064
Dinei A. Rockenbach <dineiar@gmail.com>
2018-11-24 21:14:40 -0200

    Ruy BlackNurse working

--- RyuBlackNurseDetect.py
+++ RyuBlackNurseDetect.py
@@ -12,6 +12,9 @@ class BlackNurseAwareSwitch(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(BlackNurseAwareSwitch, self).__init__(*args, **kwargs)
+        self.timeout_check = 5 #seconds
+        self.packets_per_sec_threshold = 10
+        self.timeout_block = 60 #seconds
         # self.packetslastsec = 0
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -30,32 +33,28 @@ class BlackNurseAwareSwitch(app_manager.RyuApp):
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
-
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
+        
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst, idle_timeout=5, hard_timeout=5, flags=ofproto.OFPFF_SEND_FLOW_REM)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst, idle_timeout=5, hard_timeout=5, flags=ofproto.OFPFF_SEND_FLOW_REM)
+        mod = parser.OFPFlowMod(datapath=datapath, priority=0,
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
 
-    
+    def get_src_dst(msg_data):
+        pkt = packet.Packet(msg_data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        return eth.src,eth.dst
+
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        ofp_parser = dp.ofproto_parser
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-        out_port = ofp.OFPP_FLOOD
+        out_port = ofproto.OFPP_FLOOD
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
@@ -63,57 +62,77 @@ class BlackNurseAwareSwitch(app_manager.RyuApp):
         dst = eth.dst
         src = eth.src
 
-        # self.logger.info("packet in %s %s", dp.id, msg.in_port)
-        self.logger.info("packet in %s %s %s %s", dp.id, src, dst, in_port)
+        # self.logger.info("packet in %s %s", datapath.id, msg.in_port)
+        self.logger.info("packet in %s %s %s %s", datapath.id, src, dst, in_port)
+
+        send_packet_out = True
 
         icmppkt = pkt.get_protocols(icmp.icmp)
         if icmppkt != []:
             if (icmppkt[0].type == 3 and icmppkt[0].code == 3 and isinstance(icmppkt[0].data, icmp.dest_unreach)):
                 print "> Very likely a BlackNurse packet. Add a flow for the next 5 seconds: ", icmppkt[0]
 
-                actions = [ofp_parser.OFPActionOutput(out_port)]
-                match = ofp_parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, icmpv4_type=3, icmpv4_code=3)
+                actions = [parser.OFPActionOutput(out_port)]
+                inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+                
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, eth_type=0x800, ip_proto=1, icmpv4_type=3, icmpv4_code=3)
                 # verify if we have a valid buffer_id, if yes avoid to send both
                 # flow_mod & packet_out
 
                 print(match)
 
-                if msg.buffer_id != ofp.OFP_NO_BUFFER:
-                    self.add_flow(dp, 1, match, actions, msg.buffer_id)
-                    return
+                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                    mod = parser.OFPFlowMod(datapath=datapath, buffer_id=msg.buffer_id, priority=1, match=match,
+                        instructions=inst, hard_timeout=self.timeout_check, flags=ofproto.OFPFF_SEND_FLOW_REM)
+                    send_packet_out = False
                 else:
-                    self.add_flow(dp, 1, match, actions)
+                    mod = parser.OFPFlowMod(datapath=datapath, priority=1, match=match, 
+                        instructions=inst, hard_timeout=self.timeout_check, flags=ofproto.OFPFF_SEND_FLOW_REM)
+                
+                datapath.send_msg(mod)
         
-        # print("========================\n")
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
-        out = ofp_parser.OFPPacketOut(
-            datapath=dp, buffer_id=msg.buffer_id, in_port=in_port,
-            actions=actions)
-        dp.send_msg(out)
+        if send_packet_out:
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
+            datapath.send_msg(out)
 
     @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
     def flow_removed_handler(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         match = msg.match
 
-        print("Flow REMOVED")
+        src = match['eth_src']
 
-        if msg.reason == ofp.OFPRR_IDLE_TIMEOUT or msg.reason == ofp.OFPRR_HARD_TIMEOUT:
-            print "Flow timeout"
         
-        print 'OFP v3?', ofp.OFP_VERSION==ofproto_v1_3.OFP_VERSION, ofp.OFP_VERSION
-        print str(match)
-        if 'icmpv4_type' in match:
-            print 'icmpv4_type: ', match['icmpv4_type']
-            # help(match)
+        if ofproto.OFP_VERSION == ofproto_v1_3.OFP_VERSION:
+            if msg.reason == ofproto.OFPRR_HARD_TIMEOUT: # or msg.reason == ofproto.OFPRR_IDLE_TIMEOUT:
+                self.logger.info("Flow removed, OpenFlow v1.3, removed by timeout")
+                # print str(match)
+                if 'icmpv4_type' in match and 'icmpv4_code' in match:
+                    if match['icmpv4_type'] == 3 and match['icmpv4_code'] == 3:
+                        packets = msg.packet_count
+                        seconds = msg.duration_sec
+                        self.logger.info("Flow matched %d packets in %d seconds: %d packets per sec", packets, seconds, packets/seconds)
+                        if packets/seconds > self.packets_per_sec_threshold:
+                            self.logger.info("Blocking "+src+" for blacknurse")
 
-            if match['icmpv4_type'] == 3 and match['icmpv4_code'] == 3:
-                packets = msg.packet_count
-                seconds = msg.duration_sec
-                if packets/seconds > 1000:
-                    print "blocking "+str(match)+" for blacknurse"
-                    # add_flow(bloqueia esse mac)
+                            # Tell switch to drop the packets
+                            # https://stackoverflow.com/a/41068251/3136474
+                            inst = [parser.OFPInstructionActions(ofproto.OFPIT_CLEAR_ACTIONS, [])]
+                            match = parser.OFPMatch(eth_src=src, eth_type=0x800, ip_proto=1, icmpv4_type=3, icmpv4_code=3)
+                            mod = parser.OFPFlowMod(datapath=datapath, priority=1, match=match,
+                                instructions=inst, hard_timeout=self.timeout_block)
+                            datapath.send_msg(mod)
+                else:
+                    self.logger.info("No ICMPv4_type or ICMPv4_code fields on flow's match: %s", str(match))
+            else:
+                self.logger.info("Flow removed, reason: %s", str(msg.reason))
         else:
-            print 'No ICMPv4_type field'
+            self.logger.info("Flow removed, OpenFlow version: %d", ofproto.OFP_VERSION)
